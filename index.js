21903680ebc4e1240f9c8fa782524ee90ba05ab8
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2022-04-21 15:48:35 +0900

    [create] アップロード

---
+++ index.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const {platform,homedir} = require("os");
+const fs = require('fs');
+const request = require('request');
+
+var config_path;
+var config;
+
+function get_config () {
+    switch (platform()) {
+        case "win32":
+            config_path = path.join(homedir(), "AppData", "Roaming", "terrescot", "config.json");
+            break;
+        case "darwin":
+            config_path = path.join(homedir(), "Library", "Application Support", "terrescot", "config.json");
+            break;
+        case "linux":
+            config_path = path.join(homedir(), ".config", "terrescot", "config.json");
+            break; 
+        default:
+            config_path = path.join(homedir(), ".config", "terrescot", "config.json");
+            break;
+    }
+}
+
+module.exports = {
+    get: {
+        config_path: () => {
+            get_config();
+            return config_path;
+        },
+        port: () => {
+            get_config();
+
+            config = JSON.parse(fs.readFileSync(config_path, {
+                encoding: 'utf-8'
+            }));
+
+            return config.config.port;
+        }
+    },
+    post: {
+        read_rss: () => {
+            get_config();
+            var URL = `http://localhost:${config.config.port}/soleil_api/run_function`;
+
+            request.post({
+                uri: URL,
+                headers: { "Content-type": "application/json" },
+                json: {
+                    function: 'read_rss'
+                }
+            }, (err, res, data) => {
+                console.log('Data: \n' + data + '\nError: \n' + err);
+            });
+        },
+        alerm: () => {
+            get_config();
+            var URL = `http://localhost:${config.config.port}/soleil_api/run_function`;
+
+            request.post({
+                uri: URL,
+                headers: { "Content-type": "application/json" },
+                json: {
+                    function: 'alerm'
+                }
+            }, (err, res, data) => {
+                console.log('Data: \n' + data + '\nError: \n' + err);
+            }); 
+        }
+    }
+}
